5750c0b2bd3837b02d55e96b396755df08cdd26d
John Mayer <johnxmayer@Johns-MBP.home>
2020-01-22 20:37:47 +0100

    determined route

--- index.js
+++ index.js
@@ -79,42 +79,14 @@ var roads = [
     }
     return new VillageState("Post Office", parcels);
   };
-  
+
+  runRobot(VillageState.random(), randomRobot);
+
   var mailRoute = [
     "Alice's House", "Cabin", "Alice's House", "Bob's House",
     "Town Hall", "Daria's House", "Ernie's House",
     "Grete's House", "Shop", "Grete's House", "Farm",
     "Marketplace", "Post Office"
   ];
+
   
-  function routeRobot(state, memory) {
-    if (memory.length == 0) {
-      memory = mailRoute;
-    }
-    return {direction: memory[0], memory: memory.slice(1)};
-  }
-  
-  function findRoute(graph, from, to) {
-    let work = [{at: from, route: []}];
-    for (let i = 0; i < work.length; i++) {
-      let {at, route} = work[i];
-      for (let place of graph[at]) {
-        if (place == to) return route.concat(place);
-        if (!work.some(w => w.at == place)) {
-          work.push({at: place, route: route.concat(place)});
-        }
-      }
-    }
-  }
-  
-  function goalOrientedRobot({place, parcels}, route) {
-    if (route.length == 0) {
-      let parcel = parcels[0];
-      if (parcel.place != place) {
-        route = findRoute(roadGraph, place, parcel.place);
-      } else {
-        route = findRoute(roadGraph, place, parcel.address);
-      }
-    }
-    return {direction: route[0], memory: route.slice(1)};
-  }
